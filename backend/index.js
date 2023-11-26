9c0c78b3549f86db14557ff84f95615c1e7d09d7
sheelaadhikari <adhikarisheela1000@gmail.com>
2023-11-26 19:10:25 +0545

    api for updating book

--- backend/index.js
+++ backend/index.js
@@ -44,13 +44,53 @@ app.get("/books", async (request, response) => {
         const books = await Book.find({});
         return response.status(200).json({
             count: books.length,
-            data: books
+            data: books,
         });
     } catch (error) {
         console.log(error.message);
         response.status(500).send({ message: error.message });
     }
 });
+
+//get one book from database by id
+
+app.get("/books/:id", async (request, response) => {
+    try {
+        const { id } = request.params;
+        const book = await Book.findById(id);
+        return response.status(200).json(book);
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
+});
+
+// route for updating the book
+app.put("/books/:id", async (request, response) => {
+    try {
+        if (
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ) {
+            return response.status(400).send({
+                message: "send all the required fields: title, author, publishYear",
+            });
+        }
+        const { id } = request.params;
+        const result = await Book.findByIdAndUpdate(id, request.body);
+        if (!result) {
+            return response.status(404).json({ message: "Book not found" });
+        }
+
+        return response.status(200).send({ message: "book updated successfully" });
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
+});
+
+// route for deleting book
 
 mongoose
     .connect(mongoDBURL)
