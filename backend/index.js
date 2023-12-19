c8791427d84146b1179cc828c83b2dcb923952ae
sheelaadhikari <adhikarisheela1000@gmail.com>
2023-12-19 12:58:40 +0545

    middleware for handling for cors policy

--- backend/index.js
+++ backend/index.js
@@ -3,27 +3,37 @@ import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import { Book } from "./models/bookModel.js";
 import bookR from "./routes/bookR.js";
+import cors from "cors";
+
 const app = express();
 // middle ware for parsing body request
 app.use(express.json());
 
-app.get("/", (request, response) => {
-    console.log(request);
-    return response.status(234).send("welcome to my site");
-});
+// midddle ware for handling CORS policy
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(234).send("welcome to my site");
+});
 
 app.use("/books", bookR);
 
 mongoose
-    .connect(mongoDBURL)
-    .then(() => {
-        console.log("app connected to database");
-        app.listen(PORT, () => {
-            console.log(`App is listening to the port:  ${PORT}`);
-        });
-    })
-
-    .catch((error) => {
-        console.log(error);
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("app connected to database");
+    app.listen(PORT, () => {
+      console.log(`App is listening to the port:  ${PORT}`);
     });
+  })
+
+  .catch((error) => {
+    console.log(error);
+  });
