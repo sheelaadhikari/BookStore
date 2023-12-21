75e7132be2e884c5244a217dbd937f3626160823
sheelaadhikari <adhikarisheela1000@gmail.com>
2023-12-21 20:22:26 +0545

    created books card and table

---
+++ frontend/src/components/home/BooksTable.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const BooksTable = () => {
+  return (
+    <div>BooksTable</div>
+  )
+}
+
+export default BooksTable
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-
-import './index.css'
-import App from './App.jsx'
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-  <App/>
-  </React.StrictMode>,
-)
+  <BrowserRouter>
+      <App />
+  </BrowserRouter>
+);
